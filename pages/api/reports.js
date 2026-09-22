840cba5bd7a931c88aaa67030a3793fc5a4bc21e
zintun123 <zintun123@users.noreply.github.com>
2026-09-22 17:41:24 +0800

    Update reports.js

--- pages/api/reports.js
+++ pages/api/reports.js
@@ -53,6 +53,9 @@ function getAttendanceStatus(timeIn) {
 }
 
 export default async function handler(req, res) {
+  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+  res.setHeader("Pragma", "no-cache");
+  res.setHeader("Expires", "0");
   try {
     const { data: rows, error } = await supabase.from("daily_log").select("*").order("id", { ascending: true }).range(0, 49999);
     if (error) throw error;
